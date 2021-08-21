2e7e27fc99972dc9381c1ad7e3adb375ef904dc9
blackvual <thesteadysong@icloud.com>
2021-08-21 22:56:36 +0900

    2021/08/21 サインアップ画面にてボタン配置テスト

--- ClubSunriseCoast/Controller/SignUpViewController.swift
+++ ClubSunriseCoast/Controller/SignUpViewController.swift
@@ -9,11 +9,16 @@ import Foundation
 import UIKit
 class signUp:UIViewController {
 
-//    var MyView = SignUpView()
+    var signUpView = SignUpView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        viewSetup()
+    }
+    
+    func viewSetup() {
+        self.view = signUpView
     }
     
     
--- ClubSunriseCoast/View/SignUpView.swift
+++ ClubSunriseCoast/View/SignUpView.swift
@@ -11,9 +11,52 @@ protocol signUpDelegate {
 }
 
 class SignUpView:UIView{
-//    各パーツプロパティの宣言
-    var userNameTextField:UITextField!
-    var mailAddressTextField:UITextField!
-    var signUpButton:UIButton!
-    var signInButton:UIButton!
+  
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setLayout() {
+
+        setButtonFeild()
+    }
+    
+    func setButtonFeild() {
+//ボタン・フィールド定義
+        let signUpButton:UIButton = {
+            let returnUIButton = UIButton()
+    //コードレイアウト用の定義
+            returnUIButton.translatesAutoresizingMaskIntoConstraints = false
+    //デザイン
+            returnUIButton.layer.borderColor = UIColor.blue.cgColor
+            returnUIButton.backgroundColor = .white
+            returnUIButton.setTitle("Sign Up", for: .normal)
+    //これは遷移関数を実装した後に追記
+//            returnUIButton.addTarget(<#T##target: Any?##Any?#>, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+            return returnUIButton
+        }()
+        
+        var signInButton:UIButton!
+        var userNameTextField:UITextField!
+        var mailAddressTextField:UITextField!
+
+//add作業
+        addSubview(signUpButton)
+
+//レイアウト
+        signUpButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        signUpButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        signUpButton.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.25).isActive = true
+        signUpButton.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.03125).isActive = true
+        
+        
+    }
+    
 }
+
+
